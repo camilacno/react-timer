9c4036c7d7a3feaaeacea85736d8ec51e03bde6d
Camila Nepomuceno <camila.cno@gmail.com>
2022-11-19 14:48:34 -0300

    Start new cycle

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -21,17 +22,26 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O cliclo precisa ser de no máximo 60 minutos'),
 })
 
-// interface NewCicleFormData {
+// interface NewCycleFormData {
 //   task: string
 //   minutesAmount: number
 // }
 
 // Type creation based on the schema (many advantages, like type updated automatically
 // by changing the schema)
-type NewCicleFormData = zod.infer<typeof newCycleFormValidationSchema>
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+  id: string
+  task: string
+  minutesAmount: number
+}
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCicleFormData>({
+  const [cycles, setCycles] = useState<Cycle[]>([])
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -39,10 +49,19 @@ export function Home() {
     },
   })
 
-  function handleCreateNewCicle(data: any) {
-    console.log(data)
+  function handleCreateNewCicle(data: NewCycleFormData) {
+    const id = String(new Date().getTime())
+    const newCycle: Cycle = {
+      id: id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    }
+    setCycles((state) => [...state, newCycle])
+    setActiveCycleId(id)
+    reset()
   }
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
   const task = watch('task')
   const isSubmitDisabled = !task
 
